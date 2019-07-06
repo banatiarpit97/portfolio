22333242a859e31dd9283d99a4ab9baf5b16c0b0
arpit <banatiarpit97@yahoo.co.in>
2019-07-06 15:05:50 +0530

    moved some projects in portfolio project itslef

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -15,29 +15,29 @@ type = 'all';
   constructor(public appComponent: AppComponent, public loadingService: LoadingService) {
     this.appComponent.title = "projects";
     this.apps = [
-      { name: "Catch The Mole", img: "assets/images/mole_game.png", link: "http://arpit-banati.epizy.com/Mole-in-the-hole/", t: "games" },
+      { name: "Catch The Mole", img: "assets/images/mole_game.png", link: "./../../assets/Mole-in-the-hole/index.html", t: "games" },
       { name: "Tutorialspoint PDF", img: "assets/images/tutorialspoint-pdf.png", link: "https://github.com/banatiarpit97/tutorialspoint-pdf-download", t: "utilities" },
-      { name: "Snake Game", img: "assets/images/snake.png", link: "http://arpit-banati.epizy.com/Snake%20Game/", t: "games" },
+      { name: "Snake Game", img: "assets/images/snake.png", link: "./../../assets/Snake%20Game/index.html", t: "games" },
       { name: "MEAN Notes App", img: "assets/images/MEAN-notes.png", link: "https://angular-node-mysql-notes.herokuapp.com", t: "utilities" },
-      { name: "Stopwatch", img: "assets/images/stopwatch.png", link: "http://arpit-banati.epizy.com/Stopwatch-App/", t: "utilities" },
-      { name: "Google homepage", img: "assets/images/google.png", link: "http://arpit-banati.epizy.com/Google-copy/Google%20project/app/", t: "extras" },
+      { name: "Stopwatch", img: "assets/images/stopwatch.png", link: "./../../assets/Stopwatch-App/index.html", t: "utilities" },
+      { name: "Google homepage", img: "assets/images/google.png", link: "./../../assets/Google-copy/Google%20project/app/index.html", t: "extras" },
       { name: "Hide YouTube end cards(chrome extension)", img: "assets/images/end_cards.png", link: "https://chrome.google.com/webstore/detail/hide-youtube-end-cards/jinenhpepbpkepablpjjchejlabbpken", t: "utilities" },
       { name: "Chat App", img: "assets/images/chat.png", link: "https://banati-chat.herokuapp.com", t: "utilities" },
-      { name: "Click Counter", img: "assets/images/clickcounter.png", link: "http://arpit-banati.epizy.com/Click-Counter/click%20counter/", t: "utilities" },
-      { name: "Fruit Slice Game", img: "assets/images/fruitslice.png", link: "http://arpit-banati.epizy.com/Fruit-Slice-Game/", t: "games" },
-      { name: "Reader App", img: "assets/images/reader.png", link: "http://arpit-banati.epizy.com/Reader-App/", t: "utilities" },
-      { name: "Web Dev Course", img: "assets/images/web_course.png", link: "http://arpit-banati.epizy.com/Course-Website/", t: "extras" },
-      { name: "Maths Quiz", img: "assets/images/mathsgame.png", link: "http://arpit-banati.epizy.com/Maths-Game-Website/", t: "games" },
+      { name: "Click Counter", img: "assets/images/clickcounter.png", link: "./../../assets/Click-Counter/click%20counter/index.html", t: "utilities" },
+      { name: "Fruit Slice Game", img: "assets/images/fruitslice.png", link: "./../../assets/Fruit-Slice-Game/index.html", t: "games" },
+      { name: "Reader App", img: "assets/images/reader.png", link: "./../../assets/Reader-App/index.html", t: "utilities" },
+      { name: "Web Dev Course", img: "assets/images/web_course.png", link: "./../../assets/Course-Website/index.html", t: "extras" },
+      { name: "Maths Quiz", img: "assets/images/mathsgame.png", link: "./../../assets/Maths-Game-Website/index.html", t: "games" },
       { name: "Barebone Google Map", img: "assets/images/googlemap.png", link: "http://arpit-banati.epizy.com/New Barebone Directions App/", t: "utilities" },
-      { name: "Drawing App", img: "assets/images/draw.png", link: "http://arpit-banati.epizy.com/Drawing-App/", t: "utilities" },
-      { name: "Tetris Game", img: "assets/images/tetris.png", link: "http://arpit-banati.epizy.com/tetris-game/", t: "games" },
+      { name: "Drawing App", img: "assets/images/draw.png", link: "./../../assets/Drawing-App/index.html", t: "utilities" },
+      { name: "Tetris Game", img: "assets/images/tetris.png", link: "./../../assets/tetris-game/index.html", t: "games" },
       { name: "Notes App", img: "assets/images/notes.png", link: "http://arpit-banati.epizy.com/Notes-App/", t: "utilities" },
       { name: "Facebook Homepage", img: "assets/images/facebook.png", link: "http://arpit-banati.epizy.com/Facebook-copy/Facebook%20project/", t: "extras" },
-      { name: "Catch the Egg", img: "assets/images/hengame.png", link: "http://arpit-banati.epizy.com/catch-the-egg/", t: "games" },
-      { name: "WWE Social", img: "assets/images/wwe.png", link: "http://arpit-banati.epizy.com/Social-Plugins/", t: "extras" },
+      { name: "Catch the Egg", img: "assets/images/hengame.png", link: "./../../assets/catch-the-egg/index.html", t: "games" },
+      { name: "WWE Social", img: "assets/images/wwe.png", link: "./../../assets/Social-Plugins/index.html", t: "extras" },
       { name: "Bootsrap Form", img: "assets/images/form.png", link: "http://arpit-banati.epizy.com/Bootstrap-form/", t: "extras" },
-      { name: "App landing page", img: "assets/images/applanding.png", link: "http://arpit-banati.epizy.com/App-Landing-Page/", t: "extras" },
-      { name: "Maths Tutorial", img: "assets/images/maths_course.png", link: "http://arpit-banati.epizy.com/Maths-Tutorial-website/", t: "extras" }
+      { name: "App landing page", img: "assets/images/applanding.png", link: "./../../assets/App-Landing-Page/index.html", t: "extras" },
+      { name: "Maths Tutorial", img: "assets/images/maths_course.png", link: "./../../assets/Maths-Tutorial-website/index.html", t: "extras" }
     ];
 
   }
